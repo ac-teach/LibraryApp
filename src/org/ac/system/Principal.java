f0649f7006a5f022364ebadb1d29c6a84be9e115
Alvaro Calderon <alvarocalderon@kinal.edu.gt>
2026-07-24 11:58:23 -0600

    primer commit: se agrego la base del proyecto para la HT3

---
+++ src/org/ac/system/Principal.java
@@ -0,0 +1,29 @@
+package org.ac.system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Alvaro Calderon
+ */
+public class Principal extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //convertir .fxml en nodo raiz
+        Parent raiz = FXMLLoader.load(
+                getClass().getResource("/org/ac/view/InicioSesionView.fxml"));        
+        Scene escena = new Scene(raiz);
+        
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+    }    
+}
